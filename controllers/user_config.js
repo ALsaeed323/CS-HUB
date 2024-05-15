73a3d1eb0f879c94195acce0b8dcac7f6f8d52a3
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-05-15 22:29:22 +0300

    handle logout

--- controllers/user_config.js
+++ controllers/user_config.js
@@ -6,11 +6,13 @@ import bcrypt from 'bcrypt';
 const logout = (req, res) => {
     try {
       // Destroy the user's session
+     
       req.session.destroy(err => {
         if (err) {
           console.error('Error destroying session:', err);
           // Handle error, if needed
         } else {
+          
           // Redirect the user to the home page or any other appropriate page after logout
           res.redirect('/');
         }
