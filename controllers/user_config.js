1176dbaef6caf4d4038b69e6bd73e70552d94ba0
Alsaead Aziz <alsaead2110679@miuegypt.edu.eg>
2024-05-15 20:59:44 +0300

    handle update user profile

--- controllers/user_config.js
+++ controllers/user_config.js
@@ -1,3 +1,6 @@
+import Signup from "../models/signup_schema.js";
+import bcrypt from 'bcrypt';
+
 
 
 const logout = (req, res) => {
@@ -17,9 +20,57 @@ const logout = (req, res) => {
       // Handle error, if needed
     }
   };
+  const updateProfile = async (req, res) => {
+    try {
+      // Extracting user ID from the request parameters
+      const userId = req.params.id;
+      const saltRounds = 10;
+
+  
+      // Extracting profile data from the request body
+      const { profileName, profileEmail, profilePassword, profileConfirmPassword } = req.body;
+  
+      // Validation: Check if passwords match
+      if (profilePassword !== profileConfirmPassword) {
+        return res.status(400).send("Passwords do not match");
+      }
+  
+      // Here you can perform further validation or processing of the profile data
+  
+      // Assuming you have a User model
+      const user = await Signup.findById(userId);
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+  
+      // Update user profile fields
+      user.fullname = profileName;
+      user.mail = profileEmail;
+      
+  const hashedPassword = await bcrypt.hash(profilePassword, saltRounds);
+
+
+      if (profilePassword) {
+        // Update password only if provided
+        user.password = hashedPassword;
+        user.cpassword = hashedPassword;
+      }
+  
+      // Save the updated user object
+      await user.save();
+  
+      // Redirect or send a success response
+      res.redirect(`/profile`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error.message);
+    }
+  };
+  
+  
   
   const user_config = {
-    logout,
+    logout,updateProfile,
   };
   export default user_config;
   
--- routes/user_router.js
+++ routes/user_router.js
@@ -5,16 +5,21 @@ import { isAuth } from "../controllers/authusers.js";
 
 
 
+
 var router = Router();
 
 router.get('/signin', function(req, res) {
 res.render("pages/signin");
-
- 
 });
 
+router.get('/profile', isAuth ,function(req, res) {
+    res.render("pages/profile",{user: req.session.User });
+    });
+    
 
 router.post("/",signinform.signinform);
 router.post("/logout",user_config.logout);
+router.post("/profile/:id",user_config.updateProfile);
+
 
 export default router;
